8452a28400719eaca6cdd80fe980319cd0b98f38
rajk9200 <rajkushwaha1111@gmail.com>
2025-12-09 12:39:19 +0530

    static path

--- todoList/settings.py
+++ todoList/settings.py
@@ -23,7 +23,7 @@ import os
 SECRET_KEY = 'django-insecure-5273+4@(92jw_=)ujqe#s$xc66h6m5%5m#v%7t93a5ezq-v8)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -131,9 +131,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
